dee84d75ebf1a056c4c48a5300df4abaa2639ea1
palaarda <gametime.plz@outlook.com>
2023-12-15 00:26:51 +0300

    upload

---
+++ getPerson.js
@@ -0,0 +1,53 @@
+const personParent = document.querySelector('.row');
+async function getData() {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users");
+    const jsonParse = await data.json();
+    jsonParse.forEach(person => {
+        const id = person.id;
+        const personName = person.name;
+        const personEmail = person.email;
+        const personStreet = person.address.street;
+        const personSuite = person.address.suite;
+        const personCity = person.address.city;
+        const personZipcode = person.address.zipcode;
+        const personPhone = person.phone;
+        const personWebsite = person.website;
+        const personCompany = person.company.name;
+        const personCatchphrase = person.company.cathPhrase;
+        const personBs = person.company.bs;
+
+        const card = document.createElement('div');
+        card.classList.add('col-lg-6')
+        card.classList.add('col-xxl-3')
+        card.innerHTML = `
+                <a href="/posts.html?userId=${id}">
+                    <div>
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    <i class="bi bi-person"></i>:&nbsp; <span>${personName}</span> <br>
+                                    <i class="bi bi-envelope"></i>:&nbsp; <span>${personEmail}</span> <br>
+                                    <i class="bi bi-geo-alt"></i>:&nbsp; <br>
+                                    <p>
+                                        Street: "${personStreet}", <br>
+                                        Suite: "${personSuite}",  <br>
+                                        City: "${personCity}",    <br>
+                                        Zipcode: "${personZipcode}",  <br>
+                                    </p>
+                                    <i class="bi bi-telephone"></i>:&nbsp; <span>${personPhone}</span> <br>
+                                    <i class="bi bi-browser-chrome"></i>:&nbsp; <span>${personWebsite}</span> <br>
+                                    <i class="bi bi-building-fill"></i>:&nbsp; <br>
+                                    <p>
+                                        Name: "${personCompany}", <br>
+                                        CathPhrase: "${personCatchphrase}",  <br>
+                                        Bs: "${personBs}",    <br>
+                                    </p>
+                                </p>
+                            </div>
+                        </div>
+                    </div> 
+                    </a>    `
+    personParent.appendChild(card);
+    });
+}
+getData();
---
+++ getPersonData.js
@@ -0,0 +1,36 @@
+const urlParams = new URLSearchParams(window.location.search);
+let id = urlParams.get('userId');
+
+console.log(id);
+
+const personParent = document.querySelector('.row');
+async function getPersonData(){
+    const personData = await fetch("https://jsonplaceholder.typicode.com/posts?userId=" + id);
+    const datajson = await personData.json();
+    
+    datajson.forEach(data => {
+        const card = document.createElement('div');
+        console.log(data.title);
+        card.classList.add('col-lg-12')
+        card.innerHTML = `
+            <div>
+                <div class="card">
+                    <div class="card-body">
+                        <p class="card-text">
+                            <i class="bi bi-person"></i>:&nbsp; <span>${data.title[0].toUpperCase() + data.title.slice(1)}</span> <br>
+                            <p class="history">
+                                Criminal History: <span>${data.body[0].toUpperCase() + data.body.slice(1)}</span>
+                            </p>
+                        </p>
+                    </div>
+                </div>
+            </div>    `
+    personParent.appendChild(card);
+    });
+    if (id > 10){
+        alert("Böyle Bir Kullanıcı Yok!");
+        const manuelId= parseInt(prompt("Lütfen İd yi Doğru Belirtin."));
+        window.location.href = 'posts.html?userId=' + manuelId;
+    }
+}
+getPersonData()
